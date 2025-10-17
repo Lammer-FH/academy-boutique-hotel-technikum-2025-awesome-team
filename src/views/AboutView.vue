7114fb919a79a5985a2204874882c49ced36e9b1
Alexander <alexanderlunzer@gmail.com>
2025-10-17 18:36:50 +0200

    Texte in Home und About angepasst

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,11 +7,15 @@ import ImageCarousel from '../components/ImageCarousel.vue';
   <div class="about">
     <h1>About Hotel Vue</h1>
     <ImageCarousel/>
-    <p>Seit 1492 heißt unser Hotel Gäste aus aller Welt willkommen – unter der stetigen Obhut unseres unermüdlichen Gastgebers Vlad. Über die Jahrhunderte hinweg hat Vlad mit Hingabe und Leidenschaft dafür gesorgt, dass sich jeder Besucher bei uns wie zu Hause fühlt.
+    <p>Seit 1476 heißt unser Hotel Gäste aus aller Welt willkommen – unter der stetigen Obhut unseres unermüdlichen Gastgebers Vlad. Über die Jahrhunderte hinweg hat Vlad mit Hingabe und Leidenschaft dafür gesorgt, dass sich jeder Besucher bei uns wie zu Hause fühlt.
 Vlad ist bekannt für seine unerschütterliche Energie und seine Liebe zur Gastfreundschaft – nur beim Wein macht er eine Ausnahme. Mit einem Glas in der Hand inspiziert er die historischen Hallen, begrüßt neue Gäste und teilt Geschichten, die die reiche Geschichte unseres Hauses lebendig werden lassen.
 Unser Hotel verbindet historische Eleganz mit modernem Komfort. Die Räume atmen Geschichte, während sie gleichzeitig alles bieten, was der heutige Reisende benötigt: stilvolles Ambiente, erstklassigen Service und unvergessliche Erlebnisse.
 Ob Sie auf der Durchreise sind oder einen längeren Aufenthalt planen – bei uns erleben Sie Gastfreundschaft, die über Jahrhunderte hinweg gewachsen ist. Willkommen in einem Haus, in dem Tradition und Herzlichkeit Hand in Hand gehen.</p>
-  </div>
+<p>Das Hotel Vue bietet Ihnen zahlreiche Annehmlichkeiten, welche seine Gäste schon seit dem Spätmittelalter begeistern.</p> 
+<p>Erfrischen Sie sich an unserer renommierten <b>Juice Bar</b>, entdecken Sie verloren geglaubte Schmöcker in unserer <b>Bibliothek</b> und entpannen Sie danach in unserer patentierten <i>garlic sauna</i>... </p> 
+<p>Vlad und das <b>Hotel Vue</b>erwarteen Sie!</p>
+
+</div>
 
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,10 @@
 
 <template>
     <img id="landing-page-img" src="public/images/about/lobby2.jpg"/>
-    <h1>Seit 1492 – Willkommen im Hotel Vue</h1>
-<p>Tradition, Charme und ein Gastgeber, der niemals schläft… nur beim Wein.</p>
+    <h1>Seit 1476 – Willkommen im Hotel Vue</h1>
+<p>Tradition, Charme und ein Gastgeber, der niemals schläft….</p>
+<p>Logieren Sie stilvoll in mondänem, altehrwürdigen Ambiente,</p>
+<p>genießen Sie Ihren Aufenthalt in imperialem Flair.</p>
 <div>
     <b-button class="btn">Zimmer buchen</b-button>
     <b-button class="btn">Das Hotel entdecken</b-button>
