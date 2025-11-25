50ca00440de0cad4017ce30d6996f560acbaae58
danaSchwarz <dana.schwarz@gmx.at>
2025-11-25 20:26:47 +0100

    Package json

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -108,7 +108,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import axios from 'axios'
+
 
 const route = useRoute()
 const router = useRouter()
