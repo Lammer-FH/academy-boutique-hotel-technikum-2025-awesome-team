f84c208922f0601cf3a2a09f9c38d107b16f219b
Alexander <alexanderlunzer@gmail.com>
2026-01-20 20:09:55 +0100

    UserView added

---
+++ src/views/UserView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+const auth = useAuthStore()
+</script>
+
+<template>
+  <div class="container mt-5 pt-5">
+    <h1>My Profile</h1>
+
+    <div v-if="auth.user">
+      <p><strong>First name:</strong> {{ auth.user.firstname }}</p>
+      <p><strong>Last name:</strong> {{ auth.user.lastname }}</p>
+      <p><strong>Email:</strong> {{ auth.user.email }}</p>
+  
+    </div>
+
+    <p v-else>Loading user data…</p>
+  </div>
+</template>
